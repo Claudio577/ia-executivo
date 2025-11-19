6328d0cf157e95a35d23c2d841f05384a654359a
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 14:18:39 -0300

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,5 @@ def get_llm():
     return init_chat_model(
         model="gpt-4o-mini",
         api_key=os.getenv("OPENAI_API_KEY"),
-        temperature=0.1,
+        temperature=0.1
     )
-print(">>> OPENAI KEY DETECTADA NO STREAMLIT:", os.getenv("OPENAI_API_KEY"))
