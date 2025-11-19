e36b82aaff14b9ba6b7635ede46c938e1247dd20
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 13:34:00 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 # IMPORTAÇÃO OBRIGATÓRIA — evita conflito
 from langchain_openai.chat_models import ChatOpenAI
 
-print(">>> ChatOpenAI carregado de:", ChatOpenAI.__module__)
+print(">>> Streamlit carregou config.py")
+print(">>> OPENAI_API_KEY =", os.getenv("OPENAI_API_KEY"))
+print(">>> ChatOpenAI vindo de:", ChatOpenAI.__module__)
 
 def get_llm():
     print(">>> Executando get_llm()")
